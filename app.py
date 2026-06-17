88d7461c6ceb3902990da22a0dbb4ec7172fcf92
Mansi Yadav <mansiydv1111@gmail.com>
2026-06-18 01:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,3 +47,7 @@ y_pred
 y_test
 
 #Confusion matrix for validation or evaluation
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+accuracy_score(y_test,y_pred)
+confusion_matrix(y_test,y_pred)
+print(classification_report(y_test,y_pred))
